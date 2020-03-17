bf1cd5c06df70b517e6571780d2e7942418017e2
Captaincrogs <54841728+Captaincrogs@users.noreply.github.com>
2020-03-17 10:43:59 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,74 @@
-<html lang="en">
+<?php
+$host = '127.0.0.1';
+$db = 'netland';
+$username = 'root';
+$password = '';
 
+$dsn = "mysql:host=$host;dbname=$db";
+try {
+    // create a PDO connection with the configuration data
+    $conn = new PDO($dsn, $username, $password);
+
+    // display a message if connected to database successfully
+    if ($conn) {
+        echo "Connected to the <strong>$db</strong> database successfully!";
+    }
+} catch (PDOException $e) {
+    // report error message
+    echo $e->getMessage();
+}
+
+$series = $conn->query('select title, rating from series');
+$films = $conn->query('select titel, duur from films');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+    <style>
 
+        table, td {
+            border: 1px solid black
+        }
+
+    </style>
+</head>
 <body>
 
-<h1>Welkom op het netland beeheerders paneel</h1>
+
+<h1>Series</h1>
+<table>
+    <tr>
+        <th>Titel</th>
+        <th>Rating</th>
+    </tr>
+        <?php
+foreach ($series as $row) {?>
+            <tr>
+                <td><?php echo $row["title"] ?></td>
+                <td><?php echo $row["rating"] ?></td>
+            </tr>
+        <?php }?>
+</table>
+<h1>Films</h1>
+<table>
+    <tr>
+        <th>Titel</th>
+        <th>Duur</th>
+    </tr>
     <?php
-$host = '127.0.0.1';
-$db = 'netland';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+foreach ($films as $row) {?>
+        <tr>
+            <td><?php echo $row["titel"] ?></td>
+            <td><?php echo $row["duur"] ?></td>
+        </tr>
+    <?php }?>
+</table>
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
-try
-{
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int) $e->getCode());
-}
 
-$sql = "SELECT * FROM movies";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
 
-$data = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-$sql0 = "SELECT * FROM series";
-$stmt0 = $pdo->prepare($sql0);
-$stmt0->execute();
-
-$series0 = $stmt0->fetchAll(PDO::FETCH_OBJ);
-
-?>
-        <div class="table">
-
-        <table>
-            <h1>movies</h1>
-            <?php
-foreach ($data as $key => $value) {echo "<tr>" . "<td>" . $value->title . "</td>" . "<td>" . $value->duur . "</td>" . "</tr>";}?>
-        </table>
-        <table>
-            <h2>series</h2>
-            <?php
-foreach ($series0 as $key => $value) {echo "<tr>" . "<td>" . $value->title . "</td>" . "<td>" . $value->rating . "</td>" . "</tr>";}?>
-            </table>
-            </div>
-            </body>
-            </html>
+</body>
+</html>
